1a9692693e4a957e1b72853bd0c9d36c9bfa423f
Samuelgosilva <samuelesilva@hotmail.com>
2024-10-29 19:23:17 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,18 @@
 package application;
 
-import entities.Department;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+
+
+        Seller seller = new Seller(21,"Bob", "bob@gmail.com", 3000.0, new Date(),  obj );
+        System.out.println(seller);
+
     }
 }
