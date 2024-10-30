d3171c8a3c1d6b262bbc533d3c6b1fca8b07feae
Samuelgosilva <samuelesilva@hotmail.com>
2024-10-30 12:07:25 -0300

    findByDepartment implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,8 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
+
 /*
 
 Connection conn = null; -> faz a conexao com o banco
@@ -19,8 +21,17 @@ public class Main {
 
         System.out.println("===TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n===TEST 2: seller findByDepartment ====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
+
+
 
     }
 }
