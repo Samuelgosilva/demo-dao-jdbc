a84e5e1444b89120aee22c867c973ed3c552f5ed
Samuelgosilva <samuelesilva@hotmail.com>
2024-10-30 13:29:10 -0300

    DepartmentDaoJDBC implementation and modified DaoFactory

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,13 +1,20 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
+import model.entities.Department;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao(){
+        return  new DepartmentDaoJDBC(DB.getConnection());
     }
 }
